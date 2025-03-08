e1e9792f7dcebc6adff49d816dc8094dfa6f6668
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:11:21 +0100

    Add columns to prospect_statuses table for code, name, and description

--- database/migrations/2025_03_07_192158_create_prospect_statuses_table.php
+++ database/migrations/2025_03_07_192158_create_prospect_statuses_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('prospect_statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
