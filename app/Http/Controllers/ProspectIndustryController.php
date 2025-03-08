fd73a10943224005925494b4f89cb6252b80c997
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:11:02 +0100

    Add ProspectIndustry model, controller, and migration

---
+++ app/Http/Controllers/ProspectIndustryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProspectIndustry;
+use Illuminate\Http\Request;
+
+class ProspectIndustryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProspectIndustry $prospectIndustry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProspectIndustry $prospectIndustry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProspectIndustry $prospectIndustry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProspectIndustry $prospectIndustry)
+    {
+        //
+    }
+}
