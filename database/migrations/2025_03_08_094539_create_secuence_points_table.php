ffeb823e490b422562b6784f5ceca524674bb0e7
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:11:28 +0100

    Add secuence_points table with order, message, and scheduling fields

--- database/migrations/2025_03_08_094539_create_secuence_points_table.php
+++ database/migrations/2025_03_08_094539_create_secuence_points_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('secuence_points', function (Blueprint $table) {
             $table->id();
+            // Relación con la secuencia
+
+            // Campos
+            $table->integer('order');
+            $table->text('message');
+            $table->enum('time_type', ['daily', 'weekly', 'monthly', 'quarterly', 'dynamic']);
+            $table->enum('day', ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])->nullable();
+            $table->enum('day_of_month', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'])->nullable();
+            $table->integer('days_after_start')->nullable();
+            $table->integer('days_after_previous')->nullable();
+            $table->string('goal')->nullable();
             $table->timestamps();
         });
     }
