7850f0f91a60310f0526f019a141fe4a85a0ef38
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 13:17:24 +0100

    Add language selection feature to appearance settings

--- resources/views/livewire/settings/appearance.blade.php
+++ resources/views/livewire/settings/appearance.blade.php
@@ -1,19 +1,49 @@
 <?php
 
 use Livewire\Volt\Component;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 
 new class extends Component {
-    //
-}; ?>
+    public string $language;
+
+    public function mount()
+    {
+        $this->language = Auth::user()->language ?? config('app.locale');
+    }
+
+    public function saveLanguage()
+    {
+        $user = Auth::user();
+        $user->update(['language' => $this->language]);
+        App::setLocale($this->language);
+        session()->put('locale', $this->language);
+    }
+};
+?>
 
 <div class="flex flex-col items-start">
     @include('partials.settings-heading')
 
     <x-settings.layout :heading="__('Appearance')" :subheading=" __('Update the appearance settings for your account')">
+        <!-- Tema de la aplicación -->
         <flux:radio.group x-data variant="segmented" x-model="$flux.appearance">
             <flux:radio value="light" icon="sun">{{ __('Light') }}</flux:radio>
             <flux:radio value="dark" icon="moon">{{ __('Dark') }}</flux:radio>
             <flux:radio value="system" icon="computer-desktop">{{ __('System') }}</flux:radio>
         </flux:radio.group>
+
+        <div class="mt-8"> </div>
+
+        <flux:separator />
+
+        <div class="mb-8"> </div>
+
+        <!-- Selector de idioma en Flux -->
+        <flux:select wire:model="language" wire:change="saveLanguage" placeholder="{{ __('Choose language...') }}">
+            <flux:select.option value="en">{{ __('English') }}</flux:select.option>
+            <flux:select.option value="es">{{ __('Spanish') }}</flux:select.option>
+        </flux:select>
+
     </x-settings.layout>
 </div>
