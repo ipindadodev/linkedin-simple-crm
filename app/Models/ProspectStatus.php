c916398a028979e6bda50da699c50bc79275404d
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:28:24 +0100

    Enhance ProspectStatus model with fillable attributes and relationship to Prospects

--- app/Models/ProspectStatus.php
+++ app/Models/ProspectStatus.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProspectStatus extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['code', 'name', 'description'];
+
+    // Relationship with Prospects (One to Many)
+    // Relación con Prospects (Uno a Muchos)
+    public function prospects()
+    {
+        return $this->hasMany(Prospect::class, 'status_id');
+    }
 }
