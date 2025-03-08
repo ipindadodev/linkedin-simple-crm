d95ed4735b1ef2ca7ebd5ff94e50da8ad3e4751f
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:28:27 +0100

    Enhance Sequence model with fillable attributes and relationships to SequencePoints and Prospects

--- app/Models/Sequence.php
+++ app/Models/Sequence.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sequence extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['code', 'name', 'description'];
+
+    // Relationship with SequencePoints (One to Many)
+    // Relación con SequencePoints (Uno a Muchos)
+    public function sequencePoints()
+    {
+        return $this->hasMany(SequencePoint::class);
+    }
+
+    // Relationship with Prospects (Many to Many)
+    // Relación con Prospects (Muchos a Muchos)
+    public function prospects()
+    {
+        return $this->belongsToMany(Prospect::class, 'contact_sequences')
+                    ->withPivot('included_at')
+                    ->withTimestamps();
+    }
 }
