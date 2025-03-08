7ad1e62dceebfd7dae79fd9d603a62f2de024022
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:11:11 +0100

    Add ProspectLocation model, controller, and migration

---
+++ app/Http/Controllers/ProspectLocationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProspectLocation;
+use Illuminate\Http\Request;
+
+class ProspectLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProspectLocation $prospectLocation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProspectLocation $prospectLocation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProspectLocation $prospectLocation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProspectLocation $prospectLocation)
+    {
+        //
+    }
+}
