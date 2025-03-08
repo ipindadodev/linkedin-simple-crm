e63a305ea7c1b355ae9134831c85a3c746eb8cb1
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:28:01 +0100

    Enhance Interaction model with fillable attributes and relationship to Prospect

--- app/Models/Interaction.php
+++ app/Models/Interaction.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Interaction extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['prospect_id', 'title', 'description'];
+
+    // Relationship with Prospects (One to Many)
+    // Relación con Prospects (Uno a Muchos)
+    public function prospect()
+    {
+        return $this->belongsTo(Prospect::class);
+    }
 }
