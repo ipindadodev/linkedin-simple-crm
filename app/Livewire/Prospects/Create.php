1b367296ba13d7830c119a961405db70a810d124
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 13:09:23 +0100

    Add Livewire component for creating Prospects

---
+++ app/Livewire/Prospects/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Prospects;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.prospects.create');
+    }
+}
