3d5354ae03986ba29a31033764c599c308935d3f
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:28:15 +0100

    Enhance ProspectIndustry model with fillable attributes and relationship to Prospects

--- app/Models/ProspectIndustry.php
+++ app/Models/ProspectIndustry.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProspectIndustry extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // Relationship with Prospects (One to Many)
+    // Relación con Prospects (Uno a Muchos)
+    public function prospects()
+    {
+        return $this->hasMany(Prospect::class, 'industry_id');
+    }
 }
