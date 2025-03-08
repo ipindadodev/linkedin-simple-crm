2867bb1d9350829e3f609eb8da8f3b5f3c1d309e
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 14:03:51 +0100

    Add color column with default value to prospect statuses table

--- database/migrations/2025_03_07_192158_create_prospect_statuses_table.php
+++ database/migrations/2025_03_07_192158_create_prospect_statuses_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('code');
             $table->string('name');
             $table->text('description');
+            $table->string('color')->default('#ffffff');
             $table->timestamps();
         });
     }
