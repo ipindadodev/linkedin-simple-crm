433a35d93932bbb720fe33b293affe50bfaf7617
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:39:15 +0100

    Define custom table name for ProspectIndustry model

--- app/Models/ProspectIndustry.php
+++ app/Models/ProspectIndustry.php
@@ -9,6 +9,8 @@ class ProspectIndustry extends Model
 {
     use HasFactory;
 
+    protected $table = 'prospect_industries';
+
     protected $fillable = ['name'];
 
     // Relationship with Prospects (One to Many)
