2b77e9018d81471989c1f8051143d88a357a6ccd
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-08 11:28:20 +0100

    Enhance ProspectLocation model with fillable attributes and relationship to Prospects

--- app/Models/ProspectLocation.php
+++ app/Models/ProspectLocation.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProspectLocation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'code'];
+
+    // Relationship with Prospects (One to Many)
+    // Relación con Prospects (Uno a Muchos)
+    public function prospects()
+    {
+        return $this->hasMany(Prospect::class, 'location_id');
+    }
 }
