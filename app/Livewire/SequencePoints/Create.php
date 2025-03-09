e0ad420e853a32ffe581d531e385c2c39088d0dd
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 13:52:28 +0100

    Add Livewire component for creating sequence points with associated view

---
+++ app/Livewire/SequencePoints/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\SequencePoints;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequence-points.create');
+    }
+}
