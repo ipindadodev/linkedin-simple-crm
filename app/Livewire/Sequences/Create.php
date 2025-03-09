81b0c340b3b310a1c35f05275be747471fb821e1
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 13:52:47 +0100

    Add Livewire component for creating sequences with associated view

---
+++ app/Livewire/Sequences/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Sequences;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequences.create');
+    }
+}
