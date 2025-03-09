fa0f3801cfc12a1c73c13f3ce7cfb898e490b341
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 13:53:02 +0100

    Add Livewire component for displaying sequences index with associated view

---
+++ app/Livewire/Sequences/SequencesIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Sequences;
+
+use Livewire\Component;
+
+class SequencesIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequences.index');
+    }
+}
