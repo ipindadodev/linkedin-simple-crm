61119fbc28f6a9b4ab9894f120a25524cb414ccf
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 17:11:25 +0100

    Update render view and component for Sequence table

---
+++ app/Livewire/Sequences/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Sequences;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequences.index');
+    }
+}
--- resources/views/livewire/sequences/index.blade.php
+++ resources/views/livewire/sequences/index.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <h2 class="mb-8">{{ __('Sequences') }}</h2>
+
+    <flux:separator class="mt-8 mb-8" />
+
+    <x-flux::button as="a" href="{{ route('sequences.create') }}">
+        {{ __('Create new') }}
+    </x-flux::button>
+
+    <div class="mb-16"></div>
+
+    <livewire:components.table 
+        model="App\Models\Sequence"
+        editRoute="sequences.edit"
+        viewRoute="sequences.show"
+        :columns="[
+            'id' => __('ID'),
+            'code' => __('Code'),
+            'name' => __('Name'),
+            'description' => __('Description'),
+        ]"
+        :sortable="['name']"
+        :searchable="['name', 'code', 'description']"
+        :allowEditing="true"
+        :allowDeleting="true"
+        :allowViewing="true"
+    />
 </div>
