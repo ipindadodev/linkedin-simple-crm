e62d3335367b4681c5adde9c966944b3c768ccf4
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 11:38:57 +0100

    Update page title and add favicon for improved branding

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'Laravel' }}</title>
+<title>{{ $title ?? 'LinkedIn Simple CRM' }}</title>
+
+<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
