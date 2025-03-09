d71fc5f53ed15330addf907e544d26adc71c1340
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 11:38:17 +0100

    Add Livewire component for prospects index view

---
+++ app/Livewire/Prospects/ProspectsIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Prospects;
+
+use Livewire\Component;
+
+class ProspectsIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.prospects.index');
+    }
+}
