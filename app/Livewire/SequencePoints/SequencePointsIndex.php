1fdca354d598213e5aceaa54867593b36075dc23
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 13:52:42 +0100

    Add Livewire component for displaying sequence points with associated view

---
+++ app/Livewire/SequencePoints/SequencePointsIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\SequencePoints;
+
+use Livewire\Component;
+
+class SequencePointsIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequence-points.index');
+    }
+}
