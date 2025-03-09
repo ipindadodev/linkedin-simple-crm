e5ef3f9df7ce88f4194619ec73df2f0b4f5ad185
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 17:13:33 +0100

    Create an initial Sequence seeder to populate initial useful data.

---
+++ database/seeders/InitialSequencesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Sequence;
+
+class InitialSequencesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sequences = [
+            ['PD-CCC', 'Prospección directa', 'Campaña intensiva: Conexión - Cualificación - Cierre'],
+            ['EE-EC', 'Enriquecimiento', 'Campaña de enriquecimiento y educación: Educar y cualificar'],
+            ['MA-NI', 'Mantenimiento', 'Campaña de mantenimiento dedicada a los prospectos no interesados en este momento']
+        ];
+
+        foreach ($sequences as $sequence) {
+            Sequence::create([
+                'code' => $sequence[0],
+                'name' => $sequence[1],
+                'description' => $sequence[2],
+            ]);
+        }
+    }
+}
