161a966b0ea52b93c9a6de628b58eac2795da2f3
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-09 17:11:59 +0100

    Update database seeders for initial and demo sequences

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             ProspectStatusSeeder::class,
             ProspectLocationSeeder::class,
             ProspectIndustrySeeder::class,
+            InitialSequencesSeeder::class,
         ]);
     }    
 }
