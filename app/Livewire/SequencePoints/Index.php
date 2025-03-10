3ce3ca6958b56ea714acade021aa8c385b54fc85
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-10 14:11:32 +0100

    Add Livewire component and view for sequence points index with table display

---
+++ app/Livewire/SequencePoints/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\SequencePoints;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.sequence-points.index');
+    }
+}
--- resources/views/livewire/sequence-points/index.blade.php
+++ resources/views/livewire/sequence-points/index.blade.php
@@ -1,3 +1,32 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <h2 class="mb-8">{{ __('Sequence points') }}</h2>
+
+    <flux:separator class="mt-8 mb-8" />
+
+    <x-flux::button as="a" href="{{ route('sequence-points.create') }}">
+        {{ __('Create new') }}
+    </x-flux::button>
+
+    <div class="mb-16"></div>
+
+    <livewire:components.table 
+        model="App\Models\SequencePoint"
+        editRoute="sequence-points.edit"
+        viewRoute="sequence-points.show"
+        :columns="[
+            'id' => __('ID'),
+            'sequence_id' => __('Sequence'),
+            'order' => __('Order'),
+            'time_type' => __('Time type'),
+            'goal' => __('Goal'),
+        ]"
+        :sortable="['sequence_id', 'order', 'time_type']"
+        :searchable="['message', 'goal']"
+        :allowEditing="true"
+        :allowDeleting="true"
+        :allowViewing="true"
+        :relations="[
+            'sequence_id' => ['relation' => 'sequence', 'field' => 'name'],
+        ]"
+    />
 </div>
