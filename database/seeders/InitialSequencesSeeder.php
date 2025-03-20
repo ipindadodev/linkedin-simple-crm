2beecd513e2e02354cbb13f4853a6a4ea8d96d4e
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-20 12:43:56 +0100

    Agregar secuencia de campaña de calentamiento en el seeder de secuencias iniciales

--- database/seeders/InitialSequencesSeeder.php
+++ database/seeders/InitialSequencesSeeder.php
@@ -15,6 +15,7 @@ class InitialSequencesSeeder extends Seeder
     public function run(): void
     {
         $sequences = [
+            ['CA-IN', 'Calentamiento', 'Campaña de calentamiento: Ser visible por el prospecto amtes de conectar'],
             ['PD-CCC', 'Prospección directa', 'Campaña intensiva: Conexión - Cualificación - Cierre'],
             ['EE-EC', 'Enriquecimiento', 'Campaña de enriquecimiento y educación: Educar y cualificar'],
             ['MA-NI', 'Mantenimiento', 'Campaña de mantenimiento dedicada a los prospectos no interesados en este momento']
