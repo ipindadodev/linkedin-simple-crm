b62d4e934972ee79f09bc4a49e5d3e61c39bb7ab
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-22 14:05:25 +0100

    Agregar botón para crear nuevas ubicaciones y tabla editable en la vista de ubicaciones

--- resources/views/livewire/locations/index.blade.php
+++ resources/views/livewire/locations/index.blade.php
@@ -1,3 +1,26 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h2 class="mb-8">{{ __('Locations') }}</h2>
+
+
+    <flux:separator class="mt-8 mb-8" />
+
+    
+    <x-flux::button as="a" href="{{ route('locations.create') }}">
+        {{ __('Create a new location') }}
+    </x-flux::button>
+
+    <div class="mb-16"></div>
+
+    <livewire:components.table 
+        model="App\Models\ProspectLocation"
+        editRoute="locations.edit" 
+        :columns="[
+            'code' => __('Code'), 
+            'name' => __('Name'),
+        ]"
+        :sortable="['name', 'code']"
+        :searchable="['code', 'name']"
+        :allowEditing="true"
+        :allowDeleting="true"
+    />
 </div>
