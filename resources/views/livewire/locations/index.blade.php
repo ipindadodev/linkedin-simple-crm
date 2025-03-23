4b11a40702f97d6c5f00889729077afc413f2b93
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-23 10:23:41 +0100

    Modificar el componente de ubicación para mejorar la accesibilidad y la semántica, reemplazando un <div> por un <section> y organizando el contenido con un <header>.

--- resources/views/livewire/locations/index.blade.php
+++ resources/views/livewire/locations/index.blade.php
@@ -1,26 +1,30 @@
-<div>
-    <h2 class="mb-8">{{ __('Locations') }}</h2>
+<section>
+    <header class="mb-8">
+        <h2 class="text-2xl font-semibold text-gray-800 dark:text-gray-100">
+            {{ __('Locations') }}
+        </h2>
+    </header>
 
+    <flux:separator class="my-8" />
 
-    <flux:separator class="mt-8 mb-8" />
+    <div class="mb-6">
+        <x-flux::button as="a" href="{{ route('locations.create') }}">
+            {{ __('Create a new location') }}
+        </x-flux::button>
+    </div>
 
-    
-    <x-flux::button as="a" href="{{ route('locations.create') }}">
-        {{ __('Create a new location') }}
-    </x-flux::button>
-
-    <div class="mb-16"></div>
-
-    <livewire:components.table 
-        model="App\Models\ProspectLocation"
-        editRoute="locations.edit" 
-        :columns="[
-            'code' => __('Code'), 
-            'name' => __('Name'),
-        ]"
-        :sortable="['name', 'code']"
-        :searchable="['code', 'name']"
-        :allowEditing="true"
-        :allowDeleting="true"
-    />
-</div>
+    <div class="mb-16">
+        <livewire:components.table 
+            model="App\Models\ProspectLocation"
+            editRoute="locations.edit" 
+            :columns="[
+                'code' => __('Code'), 
+                'name' => __('Name'),
+            ]"
+            :sortable="['name', 'code']"
+            :searchable="['code', 'name']"
+            :allowEditing="true"
+            :allowDeleting="true"
+        />
+    </div>
+</section>
