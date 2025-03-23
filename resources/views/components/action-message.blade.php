753aff4ad60ca8d927d1d21174f4ccc841451bf4
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-23 10:20:21 +0100

    Modificar el componente action-message para usar un elemento <aside> y mejorar la accesibilidad, añadiendo roles y clases de estilo.

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -2,13 +2,15 @@
     'on',
 ])
 
-<div
+<aside
+    role="status"
+    aria-live="polite"
     x-data="{ shown: false, timeout: null }"
     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000); })"
     x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms
     style="display: none"
-    {{ $attributes->merge(['class' => 'text-sm']) }}
+    {{ $attributes->merge(['class' => 'text-sm text-green-600 dark:text-green-400']) }}
 >
     {{ $slot->isEmpty() ? __('Saved.') : $slot }}
-</div>
+</aside>
