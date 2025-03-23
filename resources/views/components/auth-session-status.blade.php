67a109d84f1987e6dc414c1fbe82b1861902b004
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-23 10:20:52 +0100

    Modificar el componente auth-session-status para usar un elemento <p> en lugar de <div>, mejorando la accesibilidad con atributos ARIA.

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -3,7 +3,7 @@
 ])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+    <p {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600', 'role' => 'status', 'aria-live' => 'polite']) }}>
         {{ $status }}
-    </div>
+    </p>
 @endif
