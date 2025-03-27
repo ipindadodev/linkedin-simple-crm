b162313a22ca5828e45b5681933da9a17b46e995
Ignacio Pindado <127496986+ipindadodev@users.noreply.github.com>
2025-03-27 09:27:30 +0100

    Eliminar la clave foránea en la tabla de interacciones y cambiar a un campo unsignedBigInteger para prospect_id

--- database/migrations/2025_03_07_192128_create_interactions_table.php
+++ database/migrations/2025_03_07_192128_create_interactions_table.php
@@ -6,26 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('interactions', function (Blueprint $table) {
             $table->id();
-            // Relationship with the prospects table
-            $table->foreignId('prospect_id')->constrained('prospects')->cascadeOnDelete();
-            
+            $table->unsignedBigInteger('prospect_id');
             $table->string('title');
             $table->text('description');            
             $table->timestamps();
         });
-        
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('interactions');
